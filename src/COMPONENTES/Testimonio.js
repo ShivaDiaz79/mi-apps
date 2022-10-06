17cb92f3674e47aed7a523ffb13b0ca156fc744b
storatus <sebastian.glahn89@web.de>
2022-10-06 16:49:14 -0400

    first push Sebastian

--- src/COMPONENTES/Testimonio.js
+++ src/COMPONENTES/Testimonio.js
@@ -1,23 +1,24 @@
 import React from "react";
 import "../hojas-de-estilo/Testimonio.css";
 
-function Testimonio(props) {
+function Testimonio({ imagen, nombre, pais, cargo, empresa, testimonio }) {
   return (
     <div className="contenedor-testimonio">
-        <img 
-         className="imagen-testimonio"
-        src= {require(`../imagenes/proyecto-${props.imagen}.png`)}
-        alt= "Foto de Emma"/>
-        <div className="contenedor-texto-testimonio">
-          <p className="nombre-testimonio">
-            <strong>{props.nombre} </strong>en {props.pais}</p>
-          <p className="cargo-testimonio">
-            {props.cargo} en<strong> {props.empresa}</strong></p>
-          <p className="texto-testimonio">"{props.testimonio}"</p>
-
-        </div>
+      <img
+        className="imagen-testimonio"
+        src={require(`../imagenes/proyecto-${imagen}.png`)}
+        alt="Foto de Emma"
+      />
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">
+          <strong>{nombre} </strong>en {pais}
+        </p>
+        <p className="cargo-testimonio">
+          {cargo} en<strong> {empresa}</strong>
+        </p>
+        <p className="texto-testimonio">"{testimonio}"</p>
+      </div>
     </div>
-    
   );
 }
 export default Testimonio;
